21c080adbc1b5629272db05a07e3af51808a9f79
josiascarnot <josiascarnot13@gmail.com>
2023-07-18 12:21:53 +0100

    version180723

--- exemple/Send_Data/Send_Data.ino
+++ exemple/Send_Data/Send_Data.ino
@@ -4,9 +4,8 @@
  * 3 pour string
  */
 #include <YoupiLabEsp8266.h>
-#include "DHT.h"
 
-DHT dht;
+
 
 const char* ssid =  "Mon point d'acces";//
 const char* password = "12344566789";//
@@ -21,19 +20,19 @@ void setup() {
   if (esp.veriyAndConnectToWifi(ssid, password) == 1) {
     Serial.println("Connexion reussi");
   }
-  dht.setup(2);
+ 
   pinMode(pho_res, INPUT);
 }
 
 void loop() {
 
 int lum=analogRead(A0);//lecture et recuperation du niveau de lumiere
-float hum= dht.getHumidity();//lecture et recuperation de l'humidité
-float temp= dht.getTemperature();//lecture et recuperation de la temperature
+float n1= 13.41;//nombre à virgule n1
+float n2=3.14;//nombre à virgule n2
  d="YoupiLab";//string
  
   //pour envoyer des données sur la plateforme
-  if (esp.Send_Data(4, 1, lum, 3, d, 2, temp, 2, hum) == 1) {
+  if (esp.Send_Data(4, 1, lum, 3, d, 2,n1, 2, n2) == 1) {
     Serial.println("Envoie reussi");
   }
 
